8826c34f7e68205814a5ddf55a0de89780f4999b
Bryant Plano <bryant+dev@ionic.io>
2019-04-25 15:06:56 -0500

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DatePicker } from '@ionic-native/date-picker/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  currentDate = Date.now();
+
+  constructor(private datePicker: DatePicker) { }
+
+  showDateOfBirth(event: Event): void {
+
+    console.log('HomePage#showDateOfBirth');
+    event.stopPropagation();
+
+    this.datePicker.show({
+      date: this.currentDate,
+      maxDate: this.currentDate,
+      mode: 'date',
+      androidTheme: this.datePicker.ANDROID_THEMES.THEME_HOLO_LIGHT,
+      allowFutureDates: false
+    });
+
+    //.then((value: Date) => value.toLocaleDateString())
+  }
 
 }
